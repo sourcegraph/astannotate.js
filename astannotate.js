32cccdd01f0a14f2d9eb3a6fd8adf7418f86f4c7
Quinn Slack <qslack@qslack.com>
2013-06-24 15:10:58 -0700

    rangeVisitor

--- astannotate.js
+++ astannotate.js
@@ -15,3 +15,22 @@ exports.nodeVisitor = function(directive, test, c) {
     }
   };
 };
+
+// rangeVisitor constructs a range visitor function that takes a source code
+// string and optional AST node and calls c(range, params...) each time a pair
+// of comments /*directive*/.../*directive:params...*/ appears in the source
+// AST. The range object passed to the callback is of the form {start, end,
+// src, node}, where node is non-null only if the range exactly contains an AST
+// node that passes the test function.
+exports.rangeVisitor = function(directive, test, c) {
+  return function(src, ast) {
+    if (!ast) ast = acorn.parse(src);
+    var re = new RegExp('(/\\*' + directive + '\\*/)(.*?)/\\*' + directive + ':(.*?)\\*/', 'g'), m;
+    while ((m = re.exec(src)) !== null) {
+      var start = m[1].length + m.index, end = start + m[2].length;
+      var node = walk.findNodeAt(ast, start, end, test, walkall.traversers);
+      var range = {start: start, end: end, node: node && node.node, src: m[2]};
+      c(range, m[3]);
+    }
+  };
+};
--- test/test.js
+++ test/test.js
@@ -25,3 +25,39 @@ describe('AST node annotations', function() {
     })('var a/*#:A*/;');
   });
 });
+
+describe('AST range annotations', function() {
+  it('calls the registered callback function with the enclosed node', function(done) {
+    astannotate.rangeVisitor('##', null, function(range, x) {
+      range.node.type.should.eql('Identifier');
+      range.src.should.eql('a');
+      x.should.eql('A');
+      done();
+    })('var /*##*/a/*##:A*/;');
+  });
+  it('calls the registered callback function with the range if the range does not exactly contain a node', function(done) {
+    astannotate.rangeVisitor('##', null, function(range, x) {
+      range.start.should.eql(10);
+      range.end.should.eql(12);
+      range.src.should.eql('a;');
+      should.not.exist(range.node);
+      x.should.eql('A');
+      done();
+    })('var /*##*/a;/*##:A*/');
+  });
+  it('triggers on multiple instances', function(done) {
+    var xs = ['A', 'B'], types = ['Identifier', 'CallExpression'], i = 0;
+    astannotate.rangeVisitor('##', null, function(range, x) {
+      range.node.type.should.eql(types[i]);
+      x.should.eql(xs[i]);
+      i++;
+      if (i == 1) done();
+    })('var /*##*/a/*##:A*/; /*##*/a()/*##:B*/;');
+  });
+  it('respects the test function', function(done) {
+    astannotate.rangeVisitor('##', 'VariableDeclarator', function(range, x) {
+      range.node.type.should.eql('VariableDeclarator');
+      done();
+    })('var /*##*/a/*##:A*/;');
+  });
+});
